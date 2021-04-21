35f53855ed1129ba1b3b844e236d77b1a6322ff3
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-04-20 22:28:41 -0400

    Rough out structure of tests

---
+++ cypress/integration/game.spec.js
@@ -0,0 +1,37 @@
+describe('Basic game functionality', () => {
+  it('should allow the player to select a single card', () => {
+
+  });
+
+  it('should allow matching pairs to remain face-up', () => {
+
+  });
+
+  it('should flip non-matching pairs face-down', () => {
+
+  });
+});
+
+describe('Countdown functionality', () => {
+  it('should start the countdown when the user selects the first card', () => {
+
+  });
+
+  it('should flip the cards face-down when the countdown runs out', () => {
+
+  });
+
+  it('should reset the countdown when it runs out', () => {
+
+  });
+
+  it('should reset the countdown when the user selects a second card', () => {
+
+  });
+});
+
+describe('Endgame functionality', () => {
+  it('should display a congratulatory message when the player successfully matches all of the cards', () => {
+
+  });
+});
